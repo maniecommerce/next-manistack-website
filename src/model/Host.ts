96179a30ccab584982ecc68ddf8d07882252f583
maniecommerce <quaremani@gmail.com>
2025-11-15 13:38:51 +0530

    Model Creation User Host Root

---
+++ src/model/Host.ts
@@ -0,0 +1,62 @@
+import mongoose, {Schema, Document} from "mongoose"
+import {Message,MessageSchema} from "./Message"
+
+
+
+// Host Insterface whith type sefty
+export interface Host extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date; 
+  isVerified: boolean;
+  isAcceptingMessages: boolean;
+  messages: Message[];
+}
+
+
+// Host Schema for mongodb
+const HostSchema: Schema<Host> = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please use a valid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, 'Verify Code is required'],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, 'Verify Code Expiry is required'],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessages: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
+
+
+// Check new data base creation and connect alreay data base
+const HostModel =
+  (mongoose.models.Host as mongoose.Model<Host>) ||
+  mongoose.model<Host>('Host', HostSchema);
+
+export default HostModel;
---
+++ src/model/Root.ts
@@ -0,0 +1,62 @@
+import mongoose, {Schema, Document} from "mongoose"
+import {Message,MessageSchema} from "./Message"
+
+
+
+// Root Insterface whith type sefty
+export interface Root extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date; 
+  isVerified: boolean;
+  isAcceptingMessages: boolean;
+  messages: Message[];
+}
+
+
+// Root Schema for mongodb
+const RootSchema: Schema<Root> = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please use a valid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, 'Verify Code is required'],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, 'Verify Code Expiry is required'],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessages: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
+
+
+// Check new data base creation and connect alreay data base
+const RootModel =
+  (mongoose.models.Root as mongoose.Model<Root>) ||
+  mongoose.model<Root>('Root', RootSchema);
+
+export default RootModel;
